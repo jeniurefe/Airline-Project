7fb0638bbda870d66512f06af4b375d14bd6ea71
Sofía <sofirv2901@gmail.com>
2024-11-28 16:18:33 -0600

    se arreglaron las cartas de las recomendaciones

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -133,11 +133,7 @@ export const Home = () => {
 			<div className="Recomendations-Hotels container mt-5 border border-danger border-0 rounded">
 				<h1 className="text-start p-2">Alojamientos destacados</h1>
 				<div className="Hotels-Cards pb-3">
-					<RecomendationsCards /> {/*Hay que mapear la carta, para que conforme se vayan agregando, vayan apareciendo las cartas */}
-				</div>
-				<h1 className="text-start p-2">Destinos recomendados</h1>
-				<div className="Destinations-Cards pb-3 ">
-					<div id="carouselRecomendations" className="carousel slide">
+				<div id="carouselRecomendations" className="carousel slide">
 						<div className="carousel-inner ">
 							<div className="carousel-item active">
 
@@ -168,18 +164,8 @@ export const Home = () => {
 							</button>
 						</div>
 					</div>
-
 				</div>
-			</div>
-			<div className="Recomendations-Hotels container mt-5 border border-danger border-0 rounded">
-				<h1 className="text-start p-2">Alojamientos destacados</h1>
-				<div className="Hotels-Cards pb-3">
-					<div className="overflow-auto">
-						<div className="row d-flex flex-row flex-nowrap">
-							<RecomendationsCards /> <RecomendationsCards />  <RecomendationsCards /> <RecomendationsCards />  <RecomendationsCards /> <RecomendationsCards /> {/*Hay que mapear la carta, para que conforme se vayan agregando, vayan apareciendo las cartas */}
-						</div>
-					</div>
-				</div>
+				
 			</div>
 			<div className="Recomendations-Cars container mt-5 border border-danger border-0 rounded">
 				<h1 className="text-start p-2">Carros de alquiler recomendados</h1>
