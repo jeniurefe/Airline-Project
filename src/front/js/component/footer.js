a72e1ba11f0824d78ed39df36322e5e5f9a1e6c6
Sofía <sofirv2901@gmail.com>
2024-11-30 17:29:03 -0600

    logre arreglar uno de los links del footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,29 +2,30 @@ import React, { Component } from "react";
 import "../../styles/home.css";
 
 export const Footer = () => (
-	<footer className="footer mt-5 py-3">
-	 <div className="row align-items-start container ">
-		<div className="First-Column col">
-		<h5 className="m-3 text-light">Sobre nosotros</h5>
-		<a href="https://i.pinimg.com/236x/99/17/b8/9917b8cbf2a4183bfe58eee29a0f139c.jpg" className="About-Us m-3 text-light" >Acerca de *nombredepágina.com*</a>
-		<p className="m-3 text-light">Términos y condiciones</p>
-		
+ <footer className="footer mt-5 py-3">
+	 <div className="container">
+	     <div className="row align-items-start">
+		    <div className="First-Column col">
+		      <h5 className="m-3 text-light">Sobre nosotros</h5>
+		      <a href="" className="About-Us m-3" >Acerca de *nombredepágina.com*</a> {/*el link debe ser a la página correspondiente  */}
+		      <p className="m-3 text-light">Términos y condiciones</p>
+		    </div>
+		    <div className="Second-Column col">
+		      <h5 className="m-3 text-light">Contáctanos</h5>
+		      <p className="m-3 text-light">Atención al cliente</p>
+		      <p className="m-3 text-light">Garantía de servicio</p>
+		      <p className="m-3 text-light">Más información sobre servicios</p>
+		   </div>
+		    <div className="Third-Column col ">
+		      <h5 className="mt-3 text-light">Formas de pago</h5>
+		      <div className="Icons d-flex justify-content-start">
+			  <div><i className="fa-brands fa-cc-visa fa-xl m-1"></i></div>
+			  <div><i className="fa-brands fa-cc-mastercard fa-xl m-1"></i></div>
+			  <div ><i className="fa-brands fa-apple-pay fa-xl m-1"></i></div>
+			  <div><i className="fa-brands fa-cc-paypal fa-xl m-1"></i></div>
+		   </div>
 		</div>
-		<div className="Second-Column col">
-		<h5 className="m-3 text-light">Contáctanos</h5>
-		<p className="m-3 text-light">Atención al cliente</p>
-		<p className="m-3 text-light">Garantía de servicio</p>
-		<p className="m-3 text-light">Más información sobre servicios</p>
-		</div>
-		<div className="Third-Column col ">
-		<h5 className="mt-3 text-light">Formas de pago</h5>
-		<div className="Icons d-flex justify-content-start">
-			<div><i className="fa-brands fa-cc-visa fa-xl m-1"></i></div>
-			<div><i class="fa-brands fa-cc-mastercard fa-xl m-1"></i></div>
-			<div ><i class="fa-brands fa-apple-pay fa-xl m-1"></i></div>
-			<div><i class="fa-brands fa-cc-paypal fa-xl m-1"></i></div>
-		</div>
-		</div>
+	</div>
 	 </div>
 	</footer>
 );
